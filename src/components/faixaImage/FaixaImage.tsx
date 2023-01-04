567868b4b664ece8b4d778252689f4eddfcc897a
danilordev <danilordev@gmail.com>
2023-01-04 13:32:52 -0300

    att faixa 2

--- src/components/faixaImage/FaixaImage.tsx
+++ src/components/faixaImage/FaixaImage.tsx
@@ -1,12 +1,12 @@
 import {Faixa} from "./FaixaImage.style";
-// import faixaI from "../../img/fundoday.png";
+import faixaI from "../../img/fundoday.png";
 
 function FaixaImage (){
     return (
         <>
         <div>
 
-        <Faixa src="src/img/fundoday.png" />   
+        <Faixa src={faixaI} />   
         </div>
         </>
     );
